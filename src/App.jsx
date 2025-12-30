3f41de8cd28eb1fd9f02f0357aaf3e3ad69d5a7c
chirag31daorha <chirag2001daorha@gmail.com>
2025-12-30 16:56:22 +0530

    Initial commit - Brainwave UI project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Hero from './components/Hero';
+import Benefits from './components/Benefits';
+
+const App = () => {
+  return (
+    <>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header/>
+      <Hero/>
+      <Benefits/>
+    </div>
+    <ButtonGradient/>
+    </>
+  );
+};
+
+export default App;
